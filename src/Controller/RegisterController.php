97a08cb96556380b276992f369f86dc8cbe72a36
Augustin <augustin.yvon@laplateforme.io>
2024-06-27 10:49:35 +0200

    add validation to UserRegistrationFormType

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -7,7 +7,7 @@ use App\Form\UserRegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -36,7 +36,7 @@ class RegisterController extends AbstractController
         }
 
         return $this->render('register/register.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form
         ]);
     }
 
--- src/Form/UserRegistrationFormType.php
+++ src/Form/UserRegistrationFormType.php
@@ -4,20 +4,54 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserRegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le prénom ne peut pas être vide.',
+                    ]),
+                ],
+            ])
+
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'Le nom ne peut pas être vide.',
+                    ]),
+                ],
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([
+                        'message' => 'L\'email ne peut pas être vide.',
+                    ]),
+                    new Assert\Email([
+                        'message' => 'L\'email {{ value }} n\'est pas un email valide.',
+                    ]),
+                ],
+            ])
+
             ->add('password', PasswordType::class, [
                 'label' => 'Mot de passe',
+                'required' => true,
                 // Ajoutez ici des options de validation si nécessaire
             ]);
     }
