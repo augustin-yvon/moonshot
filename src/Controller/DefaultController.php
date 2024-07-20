7425ea2ea42102ff9cf5a400122a207a5ce64840
alixunderscore <alix.platier-favier@laplateforme.io>
2024-07-20 15:55:07 +0200

    fix modal pour connexion

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends AbstractController
     #[Route('', name: 'home')]
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('base.html.twig');
     }
 }
